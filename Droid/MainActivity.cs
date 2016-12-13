5dc57fc9ad15f4c03f0d6ddfcc540238734ed375
Thomas Dubiel <td@crazynexus.de>
2016-12-13 21:29:59 +0100

    adding the android application

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,13 +1,17 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
 
 namespace Hex2RGB.Droid
 {
    [Activity(Label = "Hex2RGB", MainLauncher = true, Icon = "@mipmap/icon")]
    public class MainActivity : Activity
    {
-      int count = 1;
+      EditText hexValueText;
+      TextView redTextView, greenTextView, blueTextView;
+      Button calculateButton;
+      View colorView;
 
       protected override void OnCreate(Bundle savedInstanceState)
       {
@@ -16,11 +20,32 @@ namespace Hex2RGB.Droid
          // Set our view from the "main" layout resource
          SetContentView(Resource.Layout.Main);
 
-         // Get our button from the layout resource,
-         // and attach an event to it
-         Button button = FindViewById<Button>(Resource.Id.myButton);
+         hexValueText = FindViewById<EditText>(Resource.Id.hexValueText);
+         redTextView = FindViewById<TextView>(Resource.Id.redTextView);
+         greenTextView = FindViewById<TextView>(Resource.Id.greenTextView);
+         blueTextView = FindViewById<TextView>(Resource.Id.blueTextView);
+         calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+         colorView = FindViewById<View>(Resource.Id.colorView);
 
-         button.Click += delegate { button.Text = $"{count++} clicks!"; };
+         calculateButton.Click += CalculateButton_Click;
+      }
+
+      void CalculateButton_Click(object sender, System.EventArgs e)
+      {
+         string hexValue = hexValueText.Text;
+         string redHexValue = hexValue.Substring(0, 2);
+         string greenHexValue = hexValue.Substring(2, 2);
+         string blueHexValue = hexValue.Substring(4, 2);
+
+         int redValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
+         int greenValue = int.Parse(greenHexValue, System.Globalization.NumberStyles.HexNumber);
+         int blueValue = int.Parse(blueHexValue, System.Globalization.NumberStyles.HexNumber);
+
+         redTextView.Text = redValue.ToString();
+         greenTextView.Text = greenValue.ToString();
+         blueTextView.Text = blueValue.ToString();
+
+         colorView.SetBackgroundColor(new Android.Graphics.Color(redValue, greenValue, blueValue));
       }
    }
 }
